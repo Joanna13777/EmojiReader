97653005c1b479c95632b6606d3b07c1f7bea2f8
Joanna13777 <ivansvetlov44@gmail.com>
2026-02-25 23:22:58 +0500

    Cell Swipe Action

---
+++ Emojireader/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  Emojireader
+//
+//  Created by Жанна Сергеевна  on 15/02/26.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
---
+++ Emojireader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emojireader
+//
+//  Created by Жанна Сергеевна  on 11/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
